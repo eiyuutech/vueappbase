ae2d103397b687d185921899667efdc48b1232f0
Toan Cao <caotoanbk@gmail.com>
2018-07-28 12:25:02 +0700

    add bootstrap 4

--- src/main.js
+++ src/main.js
@@ -1,5 +1,9 @@
 import Vue from 'vue'
 import App from './App.vue'
+// import jQuery from 'jquery';
+// global.jQuery = jQuery;
+// let Bootstrap = require('bootstrap');
+import 'bootstrap/dist/css/bootstrap.css';
 
 Vue.config.productionTip = false
 
