8bed0679cd03e1eadf044988005bd94ad7d15117
Toan Cao <caotoanhp2370@gmail.com>
2018-07-30 15:11:16 +0700

    add vuex

--- src/main.js
+++ src/main.js
@@ -1,12 +1,16 @@
 import Vue from 'vue'
 import App from './App.vue'
+
 // import jQuery from 'jquery';
 // global.jQuery = jQuery;
 // let Bootstrap = require('bootstrap');
 import 'bootstrap/dist/css/bootstrap.css';
 
+import store from './store'
+
 Vue.config.productionTip = false
 
 new Vue({
+  store,
   render: h => h(App)
 }).$mount('#app')
